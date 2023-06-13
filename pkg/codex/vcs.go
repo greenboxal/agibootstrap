80d0434818a3a459a828b857da26de2f49b87c79
Jonathan Lima <greenboxal@gmail.com>
2023-06-13 00:55:11 -0400

    Title: Add file committing functionality.
    - Created a new file, `pkg/codex/vcs.go`.
    - Added the `Commit` method to the `Project` struct. The method accepts a boolean whether to add all files to the commit or not.
    - On calling the `Commit` method, the function checks if the repository has any changes or not.
    - If there are no changes to commit, the function eventually returns nil.
    - Otherwise, we first get the staged changes using `GetStagedChanges` method.
    - Once we have the changes, we pass it to `gpt.PrepareCommitMessage` to get a formatted commit message.
    - Finally, we call the `fs.Commit` with the formatted commit message and `addAll` flag.
    - The function prints out the commit ID on successful commit.

---
+++ pkg/codex/vcs.go
@@ -0,0 +1,41 @@
+package codex
+
+import (
+	"fmt"
+
+	"github.com/greenboxal/agibootstrap/pkg/gpt"
+)
+
+func (p *Project) Commit(addAll bool) error {
+	isDirty, err := p.fs.IsDirty()
+
+	if err != nil {
+		return err
+	}
+
+	if !isDirty {
+		return nil
+	}
+
+	diff, err := p.fs.GetStagedChanges()
+
+	if err != nil {
+		return err
+	}
+
+	commitMessage, err := gpt.PrepareCommitMessage(diff)
+
+	if err != nil {
+		return err
+	}
+
+	commitId, err := p.fs.Commit(commitMessage, addAll)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Changes committed with commit ID %s\n", commitId)
+
+	return nil
+}
