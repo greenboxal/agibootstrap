0b57f4da72992aa070754cd23c25f700ce15fd82
Jonathan Lima <greenboxal@gmail.com>
2023-06-13 18:45:13 -0400

    z

--- pkg/vts/vts.go
+++ pkg/vts/vts.go
@@ -1,7 +1,7 @@
 package vts
 
+// _documentation_for_ai_assistant
 // TODO: Continue writing design documentation for this package
-
 // # Virtual Type System (VTS)
 // VTS is a generic type system that can be used to represent any type of data structure.
 // It's flexible enough to represent any type of data structure found in Go.
