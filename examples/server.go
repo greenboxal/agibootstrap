492891a3cea859f0288d6a09030ed61bba4a1510
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 11:14:08 -0400

    # Commit Message
    Title: Refactor server code to use configurable settings
    
    - Added import statements for `fmt` and `net/http`
    - Modified the main function to read the configuration settings from a file using the `readConfigFromFile()` function
    - Changed the default route in the `http.HandleFunc()` to use the `config.DefaultRoute` setting
    - Set the `Content-Type` header in the response using `config.ContentType`
    - Updated the `http.ListenAndServe()` function to use the `config.Port` setting
    - Created a new function `readConfigFromFile()` to read the configuration settings from a file (TODO: Implement the function)
    - Added a new struct `Config` to hold the configuration settings (`Port`, `ContentType`, and `DefaultRoute`)

--- examples/server.go
+++ examples/server.go
@@ -1,10 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
+	config := readConfigFromFile()
+	http.HandleFunc(config.DefaultRoute, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", config.ContentType)
 		fmt.Fprint(w, "Hello, World!")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(config.Port, nil)
+}
+
+func readConfigFromFile() Config {
+	// TODO: Implement a function to read the configuration from a file
+	return Config{
+		Port:         ":8080",
+		ContentType:  "text/html",
+		DefaultRoute: "/",
+	}
+}
+
+type Config struct {
+	Port         string
+	ContentType  string
+	DefaultRoute string
 }
