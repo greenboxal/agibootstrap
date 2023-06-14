16f49527011611a88cadfb4b9a43e627b5b2ea52
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 16:13:02 -0400

    # Commit Message ## Updated FTI package documentation
    - Added a detailed technical design document for the FTI package, explaining its purpose, intended usage, and package components
    - Documented the initialization process of the FTI package, including the creation of necessary directories and configuration file
    - Provided information about the update operation, which indexes files within the repository and updates the FTI
    - Included a usage example with step-by-step instructions on how to import the package, initialize a repository, update the repository, and query the repository
    - Highlighted the benefits of using the FTI package for efficient searching, duplicate detection, and similarity analysis of file-based repositories
    - Noted that the documentation provided is a high-level overview and directed users to the package's GoDoc documentation for more detailed information
    
    The changes aim to improve the usability and understanding of the FTI package. By providing comprehensive documentation, users will have a clearer picture of the package's functionalities and how to utilize them effectively.

--- pkg/fti/v2/fti.go
+++ pkg/fti/v2/fti.go
@@ -1,4 +1,123 @@
 package fti
 
-// Initialization function
-func init() {}
+/*
+// Technical Design Document - File Tree Index (FTI) Package
+//
+// Package Purpose:
+// The File Tree Index (FTI) package provides functionality for indexing file-based repositories using embedding vectors. It aims to create a spatially efficient, content-addressable mapping of files within a repository to unique embedding vectors.
+//
+// Intended Usage:
+// The FTI package can be used in various applications that require indexing file-based repositories. It is particularly useful in the following scenarios:
+// 1. Searching: The FTI allows efficient searching of files based on their content using embedding vectors.
+// 2. Duplicate Detection: The FTI can help identify duplicate files within a repository by comparing their embedding vectors.
+// 3. Similarity Analysis: Using the FTI, it is possible to analyze the similarity between files based on their embedding vectors.
+//
+// Package Components:
+// The FTI package is composed of the following key components:
+// 1. Repository: Represents a file-based repository and provides operations for initializing and updating the repository.
+// 2. Index: Manages the indexing process and provides methods for updating the index with new files and querying the index based on search terms.
+// 3. Chunker: Responsible for breaking files into chunks and generating embedding vectors for each chunk of content.
+// 4. Provider: Handles interactions with external services or libraries to perform operations such as embedding generation or search.
+//
+// Package Initialization:
+// The initialization of the FTI package involves setting up the necessary infrastructure for subsequent indexing. The initFTI function creates the required directories and configuration file.
+//
+// Updating the FTI:
+// The update operation is the key process that indexes files within the repository, creating object snapshots, and updating the FTI. It can be executed manually or set to run automatically at regular intervals.
+//
+// Usage Example:
+// To utilize the FTI package, one can follow the steps below:
+//
+// 1. Import the package:
+//         import "github.com/greenboxal/agibootstrap/pkg/fti"
+//
+// 2. Initialize a repository:
+//         repo, err := fti.NewRepository(cwd)
+//         if err != nil {
+//                 panic(err)
+//         }
+//         err = repo.Init()
+//         if err != nil {
+//                 panic(err)
+//         }
+//
+// 3. Update the repository:
+//         err = repo.Update(cmd.Context())
+//         if err != nil {
+//                 panic(err)
+//         }
+//
+// 4. Query the repository:
+//         hits, err := repo.Query(cmd.Context(), args[0], 10)
+//         if err != nil {
+//                 panic(err)
+//         }
+//         for i, hit := range hits {
+//                 fmt.Printf("+ Hit %d (score = %f, ci = %d):\n%s\n", i, hit.Distance, hit.Entry.Chunk.Index, hit.Entry.Chunk.Content)
+//         }
+//
+// Summary:
+// The FTI package provides a powerful tool for indexing file-based repositories, enabling efficient searching, duplicate detection, and similarity analysis. By using embedding vectors, the package offers a content-based approach to file indexing that can be valuable in various applications.
+//
+// Please note that the above documentation serves as a high-level overview of the FTI package. For detailed information about each component, its methods, and usage, please refer to the package's GoDoc documentation.
+*/
+
+// The File Tree Index (FTI) package provides functionality for indexing file-based repositories using embedding vectors.
+// It aims to create a spatially efficient, content-addressable mapping of files within a repository to unique embedding vectors.
+// The package can be used in various applications that require indexing file-based repositories, such as searching, duplicate detection, and similarity analysis.
+
+// Package Components:
+// The FTI package consists of four key components:
+// 1. Repository: Represents a file-based repository and provides operations for initializing and updating the repository.
+// 2. Index: Manages the indexing process and provides methods for updating the index with new files and querying the index based on search terms.
+// 3. Chunker: Responsible for breaking files into chunks and generating embedding vectors for each chunk of content.
+// 4. Provider: Handles interactions with external services or libraries to perform operations such as embedding generation or search.
+
+// Package Initialization:
+// The initialization of the FTI package involves setting up the necessary infrastructure for subsequent indexing.
+// The `initFTI` function creates the required directories and configuration file.
+
+// Updating the FTI:
+// The update operation is the key process that indexes files within the repository, creating object snapshots, and updating the FTI.
+// It can be executed manually or set to run automatically at regular intervals.
+
+// Usage Example:
+// To utilize the FTI package, follow these steps:
+
+// 1. Import the package:
+// import "github.com/greenboxal/agibootstrap/pkg/fti"
+
+// 2. Initialize a repository:
+// repo, err := fti.NewRepository(cwd)
+// if err != nil {
+//    panic(err)
+// }
+// err = repo.Init()
+// if err != nil {
+//    panic(err)
+// }
+
+// 3. Update the repository:
+// err = repo.Update(cmd.Context())
+// if err != nil {
+//    panic(err)
+// }
+
+// 4. Query the repository:
+// hits, err := repo.Query(cmd.Context(), args[0], 10)
+// if err != nil {
+//    panic(err)
+// }
+// for i, hit := range hits {
+//    fmt.Printf("+ Hit %d (score = %f, ci = %d):\n%s\n", i, hit.Distance, hit.Entry.Chunk.Index, hit.Entry.Chunk.Content)
+// }
+
+// The FTI package provides a powerful tool for indexing file-based repositories, enabling efficient searching, duplicate detection, and similarity analysis.
+// By using embedding vectors, the package offers a content-based approach to file indexing that can be valuable in various applications.
+
+// Please note that the above documentation serves as a high-level overview of the FTI package.
+// For detailed information about each component, its methods, and usage, please refer to the package's GoDoc documentation.
+
+func init() {
+
+}
