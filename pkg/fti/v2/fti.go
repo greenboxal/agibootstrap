ef9d765d97f385f9a0b127cdea473fa01d0b0a88
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 16:08:26 -0400

    # Commit Message
    [FTI Package] Implementing File Tree Index (FTI) package
    
    The changes made in this commit are as follows:
    
    - Added a design document for the FTI package, explaining its purpose and functionality.
       The design document provides an overview of the purpose of the FTI package, as well as its key features. It explains the functionality of the package in managing a File Tree Index, which is a spatially efficient, content-addressable mapping of files within a repository to unique embedding vectors. The document also outlines the package's key features, including initialization, updating, querying, indexing, storage, and performance optimization.
    
    - Implemented the `init()` function in the `fti` package.
       The `init()` function is automatically called when the package is imported. It can be used to initialize any necessary resources or perform any setup tasks. In this commit, the `init()` function is added to the `fti` package, although its implementation is currently empty.
    
    These changes aim to implement the File Tree Index (FTI) package, which will provide functionality for managing and searching file-based repositories. The FTI package allows users to easily index their repositories and retrieve files based on content similarity, enabling use cases such as code search, plagiarism detection, and data discovery.

--- pkg/fti/v2/fti.go
+++ pkg/fti/v2/fti.go
@@ -1,3 +1,24 @@
 package fti
 
-// TODO: Write a design document for the FTI package, explaining what it does and its purpose.
+// File Tree Index (FTI) Package Design
+
+/*
+The FTI package is designed to provide functionality for managing a File Tree Index. The File Tree Index is a spatially efficient, content-addressable mapping of files within a repository to unique embedding vectors. The purpose of the FTI is to enable efficient indexing and retrieval of files based on their content.
+
+The FTI package provides the following key features:
+
+1. Initialization: The package provides a function for initializing a new FTI repository. This includes creating the necessary directories and configuration files.
+
+2. Update: The package provides a function for updating the FTI repository. This involves scanning the repository, identifying new or modified files, generating embedding vectors for these files, and updating the index accordingly.
+
+3. Query: The package provides a function for querying the FTI repository based on user-defined search terms. The query operation returns a list of files ranked by relevance to the search terms.
+
+4. Indexing: The package includes functionality for generating embedding vectors for files in the repository. The indexing process utilizes external libraries and algorithms to extract features from the file content and represent them as embedding vectors.
+
+5. Storage: The package provides mechanisms for storing the index and embedding vectors efficiently. This includes data structures, compression techniques, and serialization/deserialization methods.
+
+6. Performance Optimization: The package is designed to optimize the performance of both indexing and querying operations. This includes techniques such as parallelization, caching, and query optimization.
+
+Overall, the FTI package aims to provide a scalable and efficient solution for managing and searching file-based repositories. It enables users to easily index their repositories and retrieve files based on content similarity, enabling various use cases such as code search, plagiarism detection, and data discovery.
+*/
+func init() {}
