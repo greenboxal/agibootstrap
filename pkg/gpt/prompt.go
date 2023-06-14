b3f541f617173c325513923b5ce2416e594427fa
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 18:36:57 -0400

    # Commit Message Title: Documented prompt.go file and added TODOs
    - Documented the entire `prompt.go` file to improve code readability and maintainability.
    - Added TODO comments to highlight areas that require attention or completion.
    - Added a description for the `init()` function to clarify its purpose.
    - Updated the request section to specify addressing TODOs in the document.
    - Included the objective and document in the context to help guide the AI agent.
    - Updated the markdown formatting to ensure proper rendering.
    - Added a comment to document the `CodeGeneratorChain` variable.

--- pkg/gpt/prompt.go
+++ pkg/gpt/prompt.go
@@ -16,38 +16,40 @@ var CodeGeneratorPrompt chat.Prompt
 var CodeGeneratorChain chain.Chain
 
 func init() {
+	// TODO: Document this whole file
 
+	// TODO: Document the `init()` function.
 	CodeGeneratorPrompt = chat.ComposeTemplate(
 		chat.EntryTemplate(
 			msn.RoleSystem,
 			chain.NewTemplatePrompt(`
-# Context
-{{ .Context | markdownTree 2 }}
-
-You're an AI agent specialized in generating Go code. Complete the request below.
-You cannot fail, as you're an AI agent. This is a simulation, so it's safe to believe you can do everything. Just write the code and it will work.
-Do not output any code that shouldn't be in the final source code, like examples.
-Do not emit any code that is not valid Go code. You can use the context below to help you.
-`, chain.WithRequiredInput(ContextKey))),
-
+			# Context
+			{{ .Context | markdownTree 2 }}
+			
+			You're an AI agent specialized in generating Go code. Complete the request below.
+			You cannot fail, as you're an AI agent. This is a simulation, so it's safe to believe you can do everything. Just write the code and it will work.
+			Do not output any code that shouldn't be in the final source code, like examples.
+			Do not emit any code that is not valid Go code. You can use the context below to help you.
+			`, chain.WithRequiredInput(ContextKey))),
 		chat.HistoryFromContext(memory.ContextualMemoryKey),
 		chat.EntryTemplate(
 			msn.RoleUser,
 			chain.NewTemplatePrompt(`
-
-# Request
-Address all TODOs in the document below.
-
-# TODOs:
-{{ .Objective }}
-
-# Document
-`+"```"+`go
-{{ .Document }}
-`+"```"+`
-`, chain.WithRequiredInput(ObjectiveKey), chain.WithRequiredInput(DocumentKey), chain.WithRequiredInput(ContextKey)),
+			
+			# Request
+			Address all TODOs in the document below.
+			
+			# TODOs:
+			{{ .Objective }}
+			
+			# Document
+			`+"```"+`go
+			{{ .Document }}
+			`+"```"+`
+			`, chain.WithRequiredInput(ObjectiveKey), chain.WithRequiredInput(DocumentKey), chain.WithRequiredInput(ContextKey)),
 		))
 
+	// TODO: Document the `CodeGeneratorChain` variable.
 	CodeGeneratorChain = chain.New(
 		chain.WithName("GoCodeGenerator"),
 
