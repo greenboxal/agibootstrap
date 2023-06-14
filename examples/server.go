e1bd01e5722988785bfcfea615e0093b111ce0b2
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 11:17:07 -0400

    # Commit Message
    Title: Added database functionality and configuration file support
    
    - Added function `setValue` to store key-value pairs in the database.
    - Created struct `Config` to store server configuration details.
    - Implemented function `setupDatabase` to connect to the database and perform migrations.
    - Added function `readConfigFromFile` to read server configuration from a JSON file.
    - Modified the server handler logic to handle GET requests with a specific key:
      - If the key exists in the database, return its corresponding value.
      - If the key does not exist, check for a `value` query parameter and store it in the database.
      - Update the response message accordingly.
    - Refactored the main function to use the new functions and retrieve the server configuration from the `config.json` file.
    
    The objective of these changes was to enhance the server functionality by adding database support and the ability to retrieve configuration details from an external file. This enables more flexible and customizable server behavior.

--- examples/server.go
+++ examples/server.go
@@ -2,17 +2,65 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	config := readConfigFromFile()
+	db := setupDatabase()
+
 	http.HandleFunc(config.DefaultRoute, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", config.ContentType)
-		fmt.Fprint(w, "Hello, World!")
+		key := r.URL.Query().Get("key")
+		value := getValue(db, key)
+		if value != "" {
+			fmt.Fprintf(w, "Value for key %q: %s", key, value)
+		} else {
+			value := r.URL.Query().Get("value")
+			setValue(db, key, value)
+			fmt.Fprintf(w, "Successfully set value %q for key %q", value, key)
+		}
 	})
 
 	http.ListenAndServe(config.Port, nil)
+}
+
+func setValue(db *gorm.DB, key string, value string) {
+	db.Create(&KeyValue{Key: key, Value: value})
+}
+
+type Config struct {
+	Port         string `json:"port"`
+	ContentType  string `json:"contentType"`
+	DefaultRoute string `json:"defaultRoute"`
+}
+
+func setupDatabase() *gorm.DB {
+	db, err := gorm.Open("sqlite3", "store.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&KeyValue{})
+
+	return db
+}
+
+func readConfigFromFile() Config {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	byteValue, _ := ioutil.ReadAll(file)
+
+	var config Config
+	json.Unmarshal(byteValue, &config)
+
+	return config
 }
 
 func readConfigFromFile() Config {
