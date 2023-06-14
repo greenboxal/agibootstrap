45c4b2f6350dd801430af1ba99ddf3f56aedf80a
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 00:07:57 -0400

    z

--- pkg/gpt/gpt.go
+++ pkg/gpt/gpt.go
@@ -2,6 +2,9 @@ package gpt
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/ast"
@@ -57,7 +60,7 @@ var embedder = &openai.Embedder{
 }
 var model = &openai.ChatLanguageModel{
 	Client:      oai,
-	Model:       "gpt-3.5-turbo",
+	Model:       "gpt-3.5-turbo-16k",
 	Temperature: 0.7,
 }
 
@@ -152,4 +155,85 @@ func NewSession() *Session {
 	// T2ODO: It should include (and replace) the globals `oai`, `embedder`, and `model` that are defined above.
 
 	return nil
+}
+
+var DefaultTemplateHelpers = map[string]any{
+	"json":         Json,
+	"markdownTree": MarkdownTree,
+}
+
+func init() {
+	RegisterMarkdownHelpers()
+}
+
+func RegisterMarkdownHelpers() {
+	chain.RegisterDefaultMarkdownHelpers(DefaultTemplateHelpers)
+}
+
+func Json(input any) string {
+	data, err := json.Marshal(input)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
+}
+func MarkdownTree(initialDepth int, input any) string {
+	var payload any
+
+	data, err := json.Marshal(input)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(data, &payload); err != nil {
+		panic(err)
+	}
+
+	var walk func(any, int, string, ast.Node)
+
+	walk = func(node any, depth int, key string, parent ast.Node) {
+		switch node := node.(type) {
+		case map[string]any:
+			for k, v := range node {
+				h := strings.Repeat("#", depth)
+				heading := ParseMarkdown([]byte(fmt.Sprintf("%s %s", h, k)))
+
+				walk(v, depth+1, k, heading)
+
+				ast.AppendChild(parent, heading)
+			}
+
+		case []any:
+			for _, v := range node {
+				walk(v, depth+1, "", parent)
+			}
+
+		default:
+			leaf := &ast.CodeBlock{
+				IsFenced: false,
+			}
+
+			str := fmt.Sprintf("%s", node)
+			lines := strings.Split(str, "\n")
+			for i, l := range lines {
+				lines[i] = "\t" + l
+			}
+			str = strings.Join(lines, "\n")
+
+			leaf.Literal = []byte(str)
+
+			ast.AppendChild(parent, leaf)
+		}
+	}
+
+	root := &ast.Document{}
+
+	walk(payload, initialDepth, "", root)
+
+	str := string(FormatMarkdown(root))
+
+	return str
 }
