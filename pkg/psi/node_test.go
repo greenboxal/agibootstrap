398e075457fce81803f03de4ac4ea1ebec7b60f5
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 16:05:25 -0400

    # Commit Message
    Add test cases for the Parent() and Children() methods of the psi.Node struct
    
    - Added a parent variable of type `*FileNode` and initialized it with a new instance of `FileNode` in the `TestParent()` test case
    - Added a child variable of type `*ASTNode` and initialized it with a new instance of `ASTNode` with the parent set to the parent variable in the `TestParent()` test case
    - Added an assertion to check if the parent of the child node is equal to the parent variable in the `TestParent()` test case
    - Added a parent variable of type `*FileNode` and initialized it with a new instance of `FileNode` in the `TestChildren()` test case
    - Added two child variables of type `*ASTNode` and initialized them with new instances of `ASTNode` with the parent set to the parent variable in the `TestChildren()` test case
    - Added a children variable and assigned the result of calling the `Children()` method on the parent variable
    - Added an assertion to check if the length of the children slice is 2 and if the first child is equal to child1 and the second child is equal to child2 in the `TestChildren()` test case

--- pkg/psi/node_test.go
+++ pkg/psi/node_test.go
@@ -6,13 +6,25 @@ func TestPsiNode(t *testing.T) {
 	t.Run("TestParent", func(t *testing.T) {
 		// Test the Parent() method of a psi.Node
 		// TODO: Add test logic for Parent() method
-		t.Errorf("TestParent failed")
+		parent := &FileNode{}
+		child := &ASTNode{parent: parent}
+
+		if child.Parent() != parent {
+			t.Errorf("TestParent failed")
+		}
 	})
 
 	t.Run("TestChildren", func(t *testing.T) {
 		// Test the Children() method of a psi.Node
 		// TODO: Add test logic for Children() method
-		t.Errorf("TestChildren failed")
+		parent := &FileNode{}
+		child1 := &ASTNode{parent: parent}
+		child2 := &ASTNode{parent: parent}
+
+		children := parent.Children()
+		if len(children) != 2 || children[0] != child1 || children[1] != child2 {
+			t.Errorf("TestChildren failed")
+		}
 	})
 
 	// Add more test cases as needed
