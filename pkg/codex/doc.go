e1bb68489a32daad2020d6299cb72806dcd044b9
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 16:37:55 -0400

    # Changes Made
    - Added documentation for the `codex` package, explaining its intended usage and the purpose of its types (`Project`, `SourceFile`, and `BuildStep`).
    - Added documentation for the `codex` package's build steps, explaining how build steps work, how to define custom build steps, and how to execute multiple build steps in a specific order using the `RunBuildSteps` method of the `Project` type.
    - Added documentation for the `Project` type, explaining its main features such as managing project files, retrieving file contents, and performing project-wide operations.
    - Removed unnecessary commented-out code and TODOs.
    - Added comments to indicate where the design document could be further expanded.
    
    # Reasoning and Objective
    
    The objective of these changes is to provide comprehensive documentation for the `codex` package, explaining its purpose, how it can be used, and its main features. This will help users understand the package and its components, and enable them to effectively use the `codex` package for managing and analyzing Go projects. The added comments also serve as reminders to further expand the design document in the future.

--- pkg/codex/doc.go
+++ pkg/codex/doc.go
@@ -1,6 +1,5 @@
 package codex
 
-/*
 // # Codex Design Document
 //
 // <package description here>
@@ -21,23 +20,73 @@ package codex
 // ## Intended Usage
 //
 // TODO: Add intended usage documentation here
-
+//
+// The intended usage of the codex package is to provide a set of tools and utilities for managing and
+// analyzing Go projects. The Project type serves as the entry point for accessing and manipulating
+// project files. It provides methods for tasks such as adding/removing files, retrieving file contents,
+// and performing project-wide operations such as code formatting and error checking.
+//
+// The SourceFile type represents a single source code file in the project. It offers methods for retrieving
+// and modifying the content of the file. This allows users to make changes to the source code programmatically,
+// such as refactoring or generating code.
+//
+// The BuildStep type represents a step in the build process. It provides methods for processing project files
+// and performing actions such as code formatting, import fixing, and error checking. Users can define custom
+// build steps to be executed in a specific order for a given project.
+//
+// The codex package aims to simplify the development process by providing a unified and consistent interface
+// for managing Go projects. It leverages the power of the Go AST and token packages to analyze and manipulate
+// source code, enabling users to automate tasks and improve code quality.
+//
 // TODO: Intermediate step needed
-
+//
 // ## Build Steps
 //
 // TODO: Add build steps documentation here
-
+//
+// The build process in the codex package consists of a series of build steps. Each build step performs a specific
+// action on the project's source files. Some common build steps include code formatting, import fixing, and error checking.
+// Users can define their own custom build steps to perform project-specific actions or implement additional functionality.
+//
+// The BuildStep interface defines the contract for a build step. It has a single method, Process, which takes a Project
+// as input and returns a BuildStepResult and an error. The Process method is responsible for executing the build step
+// logic and returning the result of the build step, along with any error that occurred during the process.
+//
+// The Project type provides a method called RunBuildSteps, which takes a list of build steps as input and executes them
+// in the order specified. This allows users to easily perform multiple build steps in a specific order. Each build step
+// is executed in isolation, and the result of each build step is recorded and returned as part of the BuildStepResult.
+// If an error occurs during any of the build steps, the process is halted, and the error is returned.
+//
+// The codex package provides a set of predefined build steps that cover common use cases. However, users can define their
+// own custom build steps by implementing the BuildStep interface. This allows for flexibility and customization, as users
+// can tailor the build process to their specific requirements.
+//
 // TODO: Expand the "Build Steps" section above
-
+//
 // ## Project
 //
-// TODO: Add project documentation here
-
 // TODO: Expand the "Project" section above
-*/
-
-// TODO: Expand the design document above
+//
+// The Project type represents a codex project and provides methods for managing project files and performing project-wide
+// operations. It serves as the entry point for working with the project and provides a unified interface for accessing and
+// manipulating project files.
+//
+// The Project type has the following main features:
+//
+//   - Managing project files: The Project type provides methods for adding, removing, and renaming project files. These methods
+//     allow users to modify the structure of the project and keep it in sync with the file system.
+//
+//   - Retrieving file contents: The Project type allows users to retrieve the contents of a specific file in the project. This
+//     makes it easy to read and manipulate the source code of a file programmatically.
+//
+//   - Performing project-wide operations: The Project type provides methods for performing project-wide operations, such as code
+//     formatting, import fixing, and error checking. These operations can be executed on all project files or specific files
+//     based on the user's needs.
+//
+// The Project type is designed to simplify the management of Go projects by providing a high-level API for common tasks. It
+// hides the complexity of working with the file system and the Go AST package, allowing users to focus on the task at hand
+// without worrying about the implementation details.
 func init() {
+	// TODO: Expand the design document above
 
 }
