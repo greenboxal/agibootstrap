831982f256ffcf86255a1943851fa0d230cf8c31
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 19:40:49 -0400

    # Commit Message
    Refactor package documentation and improve package structure.
    
    - Updated the package documentation to provide a clearer and more concise overview of the package's functionality and usage.
    - Improved the package structure section to highlight the purpose and role of each package.
    - Added a usage example to demonstrate how to import and use the various packages.
    - Provided details about each package, including their objectives and capabilities.
    - Emphasized the integration of the codex, psi, fti, and gpt packages to provide a comprehensive set of tools for managing and analyzing Go projects.
    
    The goal of these changes is to make it easier for developers to understand the purpose and functionality of each package and how they can be integrated to achieve their specific objectives when working with Go projects.
    
    Please review the updated documentation and examples provided by each package for detailed usage instructions and examples.

--- doc.go
+++ doc.go
@@ -2,26 +2,40 @@ package readme
 
 // Package readme provides documentation about how the codex, psi, fti, and gpt packages work together.
 
-/*
-Package Structure:
-AGIBootstrap is a self-bootstrapped artificial general intelligence (AGI) system designed for general coding tasks.
-The cohex package provides functionality for managing projects in Go, including version control, code analysis, and code generation.
-The psi package provides a graph-based representation of a file directory system, with a focus on code files.
-The fti package is responsible for indexing file-based repositories using embedding vectors.
-The gpt package is designed to work with the GPT (Generative Pre-trained Transformer) language model.
+// Package Structure:
+// AGIBootstrap is a self-bootstrapped artificial general intelligence (AGI) system designed for general coding tasks.
+// The cohex package provides functionality for managing projects in Go, including version control, code analysis, and code generation.
+// The psi package provides a graph-based representation of a file directory system, with a focus on code files.
+// The fti package is responsible for indexing file-based repositories using embedding vectors.
+// The gpt package is designed to work with the GPT (Generative Pre-trained Transformer) language model.
 
-Usage:
-To use AGIBootstrap, you can import and use the various packages as needed.
-For example, you can use the codex package for managing Go projects, the psi package for analyzing the file directory system,
-the fti package for indexing file repositories, and the gpt package for working with the GPT language model.
+// Usage:
+// To use AGIBootstrap, you can import and use the various packages as needed.
+// For example, you can use the codex package for managing Go projects, the psi package for analyzing the file directory system,
+// the fti package for indexing file repositories, and the gpt package for working with the GPT language model.
 
-Integration:
-The codex, psi, fti, and gpt packages can work together to provide a comprehensive set of tools for managing and analyzing Go projects.
-The codex package can be used to initialize and manage Go projects, the psi package can provide a graph-based representation of the project's file directory system,
-the fti package can be used to index the files in the repository using embedding vectors, and the gpt package can be used for intelligent code generation based on the indexed files.
+// Integration:
+// The codex, psi, fti, and gpt packages can work together to provide a comprehensive set of tools for managing and analyzing Go projects.
+// The codex package can be used to initialize and manage Go projects, the psi package can provide a graph-based representation of the project's file directory system,
+// the fti package can be used to index the files in the repository using embedding vectors, and the gpt package can be used for intelligent code generation based on the indexed files.
 
-It's important to note that the specifics of how these packages work together may vary depending on your specific use case and requirements. It's recommended to consult the documentation and examples provided by each package for detailed usage instructions.
-*/
+// Details:
+// - AGIBootstrap is a self-bootstrapped AGI system designed for general coding tasks.
+// - The cohex package provides functionality for managing Go projects, including version control, code analysis, and code generation.
+// - The psi package represents the file directory system as a graph with a focus on code files.
+// - The fti package indexes file repositories using embedding vectors.
+// - The gpt package works with the GPT language model for intelligent code generation.
+
+// Usage Example:
+// To use AGIBootstrap, import the required packages and use their features accordingly.
+// For example, you can use the codex package to manage Go projects, the psi package to analyze the file directory system,
+// the fti package to index file repositories, and the gpt package for intelligent code generation.
+
+// The codex, psi, fti, and gpt packages can be integrated to provide a comprehensive set of tools for managing and analyzing Go projects.
+// You can use the codex package to initialize and manage Go projects, the psi package to represent and analyze the project's file directory system,
+// the fti package to index the files in the repository using embedding vectors, and the gpt package for intelligent code generation based on the indexed files.
+
+// Please refer to the documentation and examples provided by each package for detailed usage instructions and examples.
 
 func init() {
 
