d3a20a491522c8d9c891ed6fa0830c5f80a355a9
Jonathan Lima <greenboxal@gmail.com>
2023-06-14 20:20:41 -0400

    # Commit Message
    Title: Updated package name in doc.go
    
    Changes:
    - Updated the package name from `cohex` to `codex` in the `doc.go` file.
      This change was made to reflect the correct package name for managing Go projects, including version control, code analysis, and code generation.
    
    - Removed redundant details in the documentation comments.
      The previously mentioned details about the `codex` package and other packages were duplicated and unnecessarily repeated.
    
    - Improved the initialization function's documentation.
      Added a TODO comment to expand the Godoc documentation for the `init` function in the `readme` package.

--- doc.go
+++ doc.go
@@ -4,7 +4,7 @@ package readme
 
 // Package Structure:
 // AGIBootstrap is a self-bootstrapped artificial general intelligence (AGI) system designed for general coding tasks.
-// The cohex package provides functionality for managing projects in Go, including version control, code analysis, and code generation.
+// The codex package provides functionality for managing projects in Go, including version control, code analysis, and code generation.
 // The psi package provides a graph-based representation of a file directory system, with a focus on code files.
 // The fti package is responsible for indexing file-based repositories using embedding vectors.
 // The gpt package is designed to work with the GPT (Generative Pre-trained Transformer) language model.
@@ -19,13 +19,6 @@ package readme
 // The codex package can be used to initialize and manage Go projects, the psi package can provide a graph-based representation of the project's file directory system,
 // the fti package can be used to index the files in the repository using embedding vectors, and the gpt package can be used for intelligent code generation based on the indexed files.
 
-// Details:
-// - AGIBootstrap is a self-bootstrapped AGI system designed for general coding tasks.
-// - The cohex package provides functionality for managing Go projects, including version control, code analysis, and code generation.
-// - The psi package represents the file directory system as a graph with a focus on code files.
-// - The fti package indexes file repositories using embedding vectors.
-// - The gpt package works with the GPT language model for intelligent code generation.
-
 // Usage Example:
 // To use AGIBootstrap, import the required packages and use their features accordingly.
 // For example, you can use the codex package to manage Go projects, the psi package to analyze the file directory system,
@@ -36,8 +29,6 @@ package readme
 // the fti package to index the files in the repository using embedding vectors, and the gpt package for intelligent code generation based on the indexed files.
 
 // Please refer to the documentation and examples provided by each package for detailed usage instructions and examples.
-
-// init initializes the readme package.
 func init() {
-	// TODO: Improve the documentation Godoc
+	// TODO: Expand the Godoc documentation
 }
