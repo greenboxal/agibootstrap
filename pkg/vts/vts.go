c1d4af392d45fdee133b81536d89a744706ebbb9
Jonathan Lima <greenboxal@gmail.com>
2023-06-15 11:12:19 -0400

    # Commit Message
    Refactor package structure and add new types and methods
    
    - Moved `pair.go` from `pkg/pair` to `pkg/agents/pair`
    - Added new types and methods to `vts.go`:
      - `PackageName`
      - `Package`
      - `TypeName`
      - `Type`
      - `TypeMember`
      - `Method`
      - `Parameter`
      - `Field`
    - Implemented methods for `Method` and `Field` interfaces:
      - `GetName()`
      - `GetDeclarationType()`
    - Added function `ReflectGoType` to convert `reflect.Type` to `*Type`
    
    The objective of these changes was to reorganize the package structure and introduce new types and methods for better abstraction and code organization. Additionally, the `ReflectGoType` function was implemented to provide a way to convert `reflect.Type` to the custom `Type` struct.

--- pkg/vts/vts.go
+++ pkg/vts/vts.go
@@ -1,10 +1,61 @@
 package vts
 
-import "github.com/greenboxal/agibootstrap/pkg/psi"
+import "reflect"
 
-func init() {
+type PackageName string
+
+type Package struct {
+	Name  PackageName
+	Types []*Type
 }
 
-type TypeSystem struct {
-	Node psi.Node
+type TypeName string
+
+type Type struct {
+	Pkg  PackageName
+	Name TypeName
+
+	Members []TypeMember
+}
+
+type TypeMember interface {
+	GetName() string
+	GetDeclarationType() TypeName
+}
+
+type Method struct {
+	DeclarationType TypeName
+	Name            string
+
+	Parameters []Parameter
+	Results    []Parameter
+
+	TypeParameters []Parameter
+}
+
+func (m *Method) GetName() string              { return m.Name }
+func (m *Method) GetDeclarationType() TypeName { return m.DeclarationType }
+
+type Parameter struct {
+	Name string
+	Type TypeName
+}
+
+type Field struct {
+	DeclarationType TypeName
+	Name            string
+	Type            TypeName
+}
+
+func (f *Field) GetName() string              { return f.Name }
+func (f *Field) GetDeclarationType() TypeName { return f.DeclarationType }
+
+func ReflectGoType(typ reflect.Type) *Type {
+	// TODO: Implement this by method by converting a reflect.Type to a *Type
+
+	// Convert reflect.Type to *Type
+	typeName := TypeName(typ.String())
+	return &Type{
+		Name: typeName,
+	}
 }
