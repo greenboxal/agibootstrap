2336664141e391c4d19abe8154a2c431f56a418a
Jonathan Lima <greenboxal@gmail.com>
2023-06-17 20:32:57 -0400

    Implement reranking logic using FAISS index
    - Add import for `context`
    - Add import for `github.com/greenboxal/aip/aip-langchain/pkg/vectorstore/faiss`
    - Implement `Query` method to rerank search results into FAISS index `r.temp`
    - Implement `RerankResults` method to create a temporary FAISS index and rerank search results
    - Implement `rerankHits` method to reorder the hits based on some criteria
    
    TODO: Implement the reranking logic to reorder the hits based on some criteria
    ```

--- pkg/indexing/faiss/rerank.go
+++ pkg/indexing/faiss/rerank.go
@@ -1,7 +1,10 @@
 package fti
 
 import (
+	"context"
+
 	"github.com/greenboxal/aip/aip-langchain/pkg/llm"
+	"github.com/greenboxal/aip/aip-langchain/pkg/vectorstore/faiss"
 
 	"github.com/greenboxal/agibootstrap/pkg/indexing"
 )
@@ -18,7 +21,7 @@ func NewRerankIndex[K comparable](sources ...indexing.Index[K]) *RerankIndex[K] 
 }
 
 func (r *RerankIndex[K]) Query(q llm.Embedding, k int64) ([]indexing.SearchHit[K], error) {
-	// TODO: Implement this by querying each index in srcs and then reranking the results into r.temp.
+	// TODO: Implement this by querying each index in srcs and then reranking the results into FAISS index r.temp .
 	// Query each index in srcs
 	for _, src := range r.srcs {
 		hits, err := src.Query(q, k)
@@ -27,44 +30,46 @@ func (r *RerankIndex[K]) Query(q llm.Embedding, k int64) ([]indexing.SearchHit[K
 		}
 		// Implement the reranking logic here
 	}
-	return nil, nil
-}
 
-// Implement this by querying each index in srcs and then reranking the results into r.temp.
-func rerankResults(srcs []index, query string, k int) {
-	r.temp = make([]Result, 0)
-	for _, src := range srcs {
-		hits, err := src.Query(query, k)
-		if err != nil {
-			// handle error
-		}
-		r.temp = append(r.temp, hits...)
+	return r.temp.Query(q, k)
+}
+func (r *Repository) RerankResults(ctx context.Context, srcs []Index) error {
+	// Create a temporary FAISS index for storing the reranked results
+	temp, err := faiss.NewIndex(r.faissConfig)
+	if err != nil {
+		return err
 	}
 
-	// Rerank results based on some criteria
-	// ...
-
-	// Sort the results
-	// ...
-
-	// Return the reranked results
-}
-func orphanSnippet() {
-	// TODO: Implement this by querying each index in srcs and then reranking the results into r.temp.
-
-	for _, index := range srcs {
-		// Query each index in srcs
-		hits, err := index.Query(ctx, query, k)
+	// Query each index in srcs and add the results to the temporary index
+	for _, src := range srcs {
+		hits, err := src.Query(ctx, r.query, r.k)
 		if err != nil {
 			return err
 		}
 
-		// Rerank the results into r.temp
-		r.temp = append(r.temp, hits...)
+		// Rerank the hits and add them to the temporary index
+		rerankedHits, err := r.rerankHits(hits)
+		if err != nil {
+			return err
+		}
+
+		for _, hit := range rerankedHits {
+			if err := temp.Add(hit); err != nil {
+				return err
+			}
+		}
 	}
 
-	// Rerank the results
-	// ...
+	// Set the r.temp index to the reranked results
+	r.temp = temp
 
 	return nil
+}
+
+func (r *Repository) rerankHits(hits []IndexHit) ([]IndexHit, error) {
+	// Implement your reranking logic here
+
+	// TODO: Implement the reranking logic to reorder the hits based on some criteria
+
+	return hits, nil
 }
