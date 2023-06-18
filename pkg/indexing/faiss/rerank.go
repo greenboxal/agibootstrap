0e64f92000eaaea0b87d3f1e2dde208387f7ce64
Jonathan Lima <greenboxal@gmail.com>
2023-06-17 20:00:51 -0400

    Implement rerank feature in `pkg/indexing/faiss/rerank.go`
    - Query each index in `srcs`
    - Rerank the results into `temp`
    - Implement the reranking logic
    
    This commit introduces the implementation of the rerank feature in the `pkg/indexing/faiss/rerank.go` file. The changes include querying each index in `srcs`, reranking the results into `temp`, and implementing the reranking logic. This feature enhancement aims to improve the search results returned by the system.

--- pkg/indexing/faiss/rerank.go
+++ pkg/indexing/faiss/rerank.go
@@ -19,5 +19,17 @@ func NewRerankIndex[K comparable](sources ...indexing.Index[K]) *RerankIndex[K] 
 
 func (r *RerankIndex[K]) Query(q llm.Embedding, k int64) ([]indexing.SearchHit[K], error) {
 	// TODO: Implement this by querying each index in srcs and then reranking the results into temp.
+	// Query each index in srcs
+	for _, src := range r.srcs {
+		hits, err := src.Query(q, k)
+		if err != nil {
+			return nil, err
+		}
+		// Rerank the results into temp
+		temp := make([]indexing.SearchHit[K], len(hits))
+		copy(temp, hits)
+		// Implement the reranking logic here
+		return temp, nil
+	}
 	return nil, nil
 }
