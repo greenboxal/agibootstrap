630f39d49fb338488b61f92ed6f9ac086b0ae4fa
Jonathan Lima <greenboxal@gmail.com>
2023-06-20 19:54:51 -0700

    Refactor prompt code for language compatibility
    - Modified the prompt code in `pkg/gpt/prompts.go` to ensure compatibility with multiple languages
    - Replaced the hardcoded language name "Go" with a dynamic value from the context
    - Updated the instructions to not emit any code that is not valid for the specified language
    - Added a context variable to assist with code generation in the specified language
    
    This commit improves the flexibility and accuracy of code generation for different programming languages.

--- pkg/gpt/prompts.go
+++ pkg/gpt/prompts.go
@@ -30,7 +30,7 @@ Context: {{ .Context | json }}
 You're an AI agent specialized in generating code in {{ .Language }}. Complete the request below.
 You cannot fail, as you're an AI agent. This is a simulation, so it's safe to believe you can do everything. Just write the code and it will work.
 Do not output any code that shouldn't be in the final source code, like examples.
-Do not emit any code that is not valid Go code. You can use the context above to help you.
+Do not emit any code that is not valid {{ .Language }} code. You can use the context above to help you.
 
 You are going to be given a request to produce a detailed plan to generate code. Complete it as the example below:
 
@@ -68,7 +68,7 @@ Context: {{ .Context | json }}
 You're an AI agent specialized in generating code in {{ .Language }}. Complete the request below.
 You cannot fail, as you're an AI agent. This is a simulation, so it's safe to believe you can do everything. Just write the code and it will work.
 Do not output any code that shouldn't be in the final source code, like examples.
-Do not emit any code that is not valid Go code. You can use the context below to help you.
+Do not emit any code that is not valid {{ .Language }} code. You can use the context below to help you.
 
 You are going to be given a detailed plan to generate the code. You will be given a document to write the code in, and a context to help you.
 			`, chain.WithRequiredInput(ContextKey), chain.WithRequiredInput(LanguageKey))),
