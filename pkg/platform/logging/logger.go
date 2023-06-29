98aec59393d7b64ba2c811b36ae2dc234dcad966
Jonathan Lima <greenboxal@gmail.com>
2023-06-29 13:47:54 -0400

    Add logger functionality to the platform package
    This commit adds a new file `logger.go` to the `pkg/platform/logging` package. The `logger.go` file contains functions for initializing and accessing the root logger, as well as creating named loggers.
    
    The `init()` function initializes the root logger using the `zap.NewDevelopmentConfig()` function from the `go.uber.org/zap` package. It sets the `DisableStacktrace` option to `false` and the `DisableCaller` option to `true`. If there is an error during initialization, the function panics.
    
    The `GetRootLogger()` function returns a pointer to the root logger.
    
    The `GetLogger(name string)` function returns a pointer to a `zap.SugaredLogger` that is named with the provided `name` parameter.
    
    These changes enable logging functionality in the platform package, making it easier to track and debug issues in the code.

---
+++ pkg/platform/logging/logger.go
@@ -0,0 +1,27 @@
+package logging
+
+import "go.uber.org/zap"
+
+var rootLogger *zap.Logger
+
+func init() {
+	config := zap.NewDevelopmentConfig()
+	config.DisableStacktrace = false
+	config.DisableCaller = true
+
+	logger, err := config.Build()
+
+	if err != nil {
+		panic(err)
+	}
+
+	rootLogger = logger
+}
+
+func GetRootLogger() *zap.Logger {
+	return rootLogger
+}
+
+func GetLogger(name string) *zap.SugaredLogger {
+	return rootLogger.Sugar().Named(name)
+}
